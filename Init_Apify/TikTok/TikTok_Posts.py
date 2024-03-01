a5d82dc8e1d1f4fa0a0255a4b5126439b6024f65
VictorHOgawa <victor.ogawa@gmail.com>
2024-03-01 13:58:37 -0400

    fix tiktok

--- Init_Apify/TikTok/TikTok_Posts.py
+++ Init_Apify/TikTok/TikTok_Posts.py
@@ -27,12 +27,11 @@ def upload_file(file_name, bucket, object_name=None):
 now = datetime.now()
 timestamp = datetime.timestamp(now)
 
-# INIT API ROUTE
 input = requests.get(f"{os.environ['API_IP']}/scrape/without/tiktok")
 
 input = input.json()
 
-input = input["profiles"]
+input = input["tiktok"]
 
 tiktok_names = [item["tiktok"] for item in input]
 
@@ -40,48 +39,43 @@ tiktok_ids = [item["id"] for item in input]
 
 client = ApifyClient(os.environ['APIFY_KEY'])
 
-for tiktok_name, tiktok_id in zip(tiktok_names, tiktok_ids):
-    
-    run_input = {
-        "disableCheerioBoost": False,
-        "disableEnrichAuthorStats": False,
-        "profiles": [tiktok_name],
-        "scrapeLastNDays": 60,
-        "resultsPerPage": 100,
-        "shouldDownloadCovers": False,
-        "shouldDownloadSlideshowImages": False,
-        "shouldDownloadVideos": False
-    }
-    run = client.actor("GdWCkxBtKWOsKjdch").call(run_input=run_input)
+run_input = {
+    "disableCheerioBoost": False,
+    "disableEnrichAuthorStats": False,
+    "profiles": [tiktok_name for tiktok_name in tiktok_names],
+    "resultsPerPage": 20,
+    "scrapeLastNDays": 60,
+    "shouldDownloadCovers": False,
+    "shouldDownloadSlideshowImages": False,
+    "shouldDownloadVideos": False
+}
+run = client.actor("GdWCkxBtKWOsKjdch").call(run_input=run_input)
 
-    json_array = []
-    json_str = ""
-    posts_str = ""
-    posts_set = set()
-    for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-        json_data = json.dumps(item, ensure_ascii=False)
-        json_array.append(json.loads(json_data))
-        
-        for item in json_array:
-            if item["webVideoUrl"]:
-                posts_set.add(item["webVideoUrl"])
+json_array = []
+json_str = ""
+posts_str = ""
+posts_set = set()
+for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+    json_data = json.dumps(item, ensure_ascii=False)
+    json_array.append(json.loads(json_data))
+    
+    for item in json_array:
+        if item["webVideoUrl"]:
+            posts_set.add(item["webVideoUrl"])
+            for tiktok_name, tiktok_id in zip(tiktok_names, tiktok_ids):
                 if tiktok_name.lower() in item["webVideoUrl"].lower():
                     item["tiktok_id"] = tiktok_id
 
-        json_str = json.dumps(json_array, ensure_ascii=False, indent=4)
-        posts_array = list(posts_set)
-        posts_str = json.dumps(posts_array, ensure_ascii=False, indent=4)
+    json_str = json.dumps(json_array, ensure_ascii=False, indent=4)
+    posts_array = list(posts_set)
+    posts_str = json.dumps(posts_array, ensure_ascii=False, indent=4)
 
-    if json_str != "":
-        with open(f"/home/scrapeops/axioon-scrape/Init_Apify/Results/TikTok/TikTok_Posts_{tiktok_name}.json", "w") as f:
-            f.write(json_str)
+with open(f"/home/scrapeops/axioon-scrape/Apify/Results/TikTok/TikTok_Posts.json", "w") as f:
+    f.write(json_str)
 
-    if posts_str != "":
-        with open(f"/home/scrapeops/axioon-scrape/Init_Apify/Results/TikTok/TikTok_Posts_Urls_{tiktok_name}.json", "w") as f:
-            f.write(posts_str)
-        
-    if json_str != "":
-        upload_file(f"/home/scrapeops/axioon-scrape/Init_Apify/Results/TikTok/TikTok_Posts_{tiktok_name}.json", "axioon", f"Apify/TikTok/Posts/TikTok_Posts_{tiktok_name}_{timestamp}.json")
+with open(f"/home/scrapeops/axioon-scrape/Apify/Results/TikTok/TikTok_Posts_Urls.json", "w") as f:
+    f.write(posts_str)
 
-    if json_str != "":
-        file_name = requests.post(f"{os.environ['API_IP']}/webhook/tiktok", json={"records": f"Apify/TikTok/Posts/TikTok_Posts_{tiktok_name}_{timestamp}.json"})
+upload_file(f"/home/scrapeops/axioon-scrape/Apify/Results/TikTok/TikTok_Posts.json", "axioon", f"Apify/TikTok/Posts/TikTok_Posts_{timestamp}.json")
+
+file_name = requests.post(f"{os.environ['API_IP']}/webhook/tiktok", json={"records": f"Apify/TikTok/Posts/TikTok_Posts_{timestamp}.json"})
